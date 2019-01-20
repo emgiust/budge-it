7d3b5f8483fe2abbd8699a0fab663391210fb9c6
Emily Giust <emilygiust@Emilys-MacBook-Pro.local>
2019-01-20 17:51:10 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './layout/Header';
+import About from './pages/About';
+import ExpenseTable from './components/ExpenseTable';
+import AddExpense from './components/AddExpense';
+import NotFound from './pages/NotFound';
+
+import { Provider } from './context';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider>
+        <Router>
+          <div className="App">
+            <Header />
+            <div className="m-5">
+              <div className="row">
+                <div className="col-xl mt-2">
+                  <Switch>
+                    <Route exact path="/" component={ExpenseTable} />
+                    <Route exact path="/expense/add" component={AddExpense} />
+                    <Route exact path="/about" component={About} />
+                    <Route component={NotFound} />
+                  </Switch>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
