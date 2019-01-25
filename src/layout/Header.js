90ed0669c89b7560704cafa59b3d15e9e211a4f0
Emily Giust <emilygiust@Emilys-MacBook-Pro.local>
2019-01-24 19:56:05 -0600

    second push for 404 when clicking on logo

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-success mb-3 py-0">
       <div className="container">
-        <h1 className="navbar-brand">Expense Tracker</h1>
+        <h1 className="navbar-brand">+Expense Tracker+</h1>
         <div>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
