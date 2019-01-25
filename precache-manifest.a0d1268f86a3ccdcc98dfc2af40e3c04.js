ac83f6eaf178a7c3d0e6512e35c254abe8de8a04
Emily Giust <emilygiust@Emilys-MacBook-Pro.local>
2019-01-24 19:49:35 -0600

    Updates

---
+++ precache-manifest.a0d1268f86a3ccdcc98dfc2af40e3c04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c594b5b6bfedf8ec159",
+    "url": "/expense-tracker/static/js/runtime~main.2c594b5b.js"
+  },
+  {
+    "revision": "e2463f2e4ffaa4b67e8d",
+    "url": "/expense-tracker/static/js/main.e2463f2e.chunk.js"
+  },
+  {
+    "revision": "11ccbe56d422d6bf44ca",
+    "url": "/expense-tracker/static/js/1.11ccbe56.chunk.js"
+  },
+  {
+    "revision": "e2463f2e4ffaa4b67e8d",
+    "url": "/expense-tracker/static/css/main.00a966cc.chunk.css"
+  },
+  {
+    "revision": "35c10c89cef49de3cf7e86f9c41919de",
+    "url": "/expense-tracker/index.html"
+  }
+];
