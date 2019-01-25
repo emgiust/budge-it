174ec3c050258e3425d7dbb9cd2c1a2706251be2
Emily Giust <emilygiust@Emilys-MacBook-Pro.local>
2019-01-24 19:53:59 -0600

    fixed 404 when clicking on logo in header

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -5,9 +5,7 @@ const Header = () => {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-success mb-3 py-0">
       <div className="container">
-        <a href="/" className="navbar-brand">
-          Expense Tracker
-        </a>
+        <h1 className="navbar-brand">Expense Tracker</h1>
         <div>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
